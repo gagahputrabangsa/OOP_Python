40f3d21202a3d8ed5fa7f37ccffc24edd40880b0
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2024-06-12 19:06:34 +0700

    Add files via upload

---
+++ p3e1.py
@@ -0,0 +1,14 @@
+class Mahasiswa:
+    def __init__(self, nama, nim, fakultas, hobi):
+        self.nama = nama
+        self.nim = nim
+        self.fakultas = fakultas
+        self.hobi = hobi
+        
+
+
+    def __str__(self):
+        return f"{self.nama} {self.nim}\n-----PROGRAM MENAMPILKAN IDENTITAS-----\nNama saya adalah {self.nama} NIM saya {self.nim}. \nSaya dari fakultas {self.fakultas}.\nHobi saya adalah {self.hobi}"
+
+mhs = Mahasiswa("Gagah Putra Bangsa", "(064002100036)", "Teknik Infomatika", "Menghasilkan Uang")
+print(mhs)
