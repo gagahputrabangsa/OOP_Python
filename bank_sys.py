322afc8a886e0ed09816bafad87d405501c48585
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2024-11-24 01:43:54 +0700

    Create bank_sys.py

---
+++ bank_sys.py
@@ -0,0 +1,5 @@
+class BankAccount:
+    def __init__(self, account_number, owner_name, balance=0):
+        self.account_number = account_number
+        self.owner_name = owner_name
+        self.balance = balance
