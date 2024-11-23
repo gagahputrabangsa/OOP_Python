e5e56cc5e9d6cd8cc8679a408ab451cd63e0570a
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2024-11-24 01:44:20 +0700

    deposit func

--- bank_sys.py
+++ bank_sys.py
@@ -3,3 +3,10 @@ class BankAccount:
         self.account_number = account_number
         self.owner_name = owner_name
         self.balance = balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.balance += amount
+            print(f"Deposited {amount}. New balance: {self.balance}")
+        else:
+            print("Deposit amount must be positive.")
