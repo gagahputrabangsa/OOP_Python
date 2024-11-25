de7c54f8c532e0b8afb0bec76b104934ac9c3a8f
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2024-11-25 14:14:59 +0700

    Create libofBook.py

---
+++ libofBook.py
@@ -0,0 +1,11 @@
+class Book:
+    def __init__(self, title, author, year):
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def display_info(self):
+        print(f"Title:   
+ {self.title}")
+        print(f"Author: {self.author}")
+        print(f"Year: {self.year}")
