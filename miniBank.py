eda4f4f8854f23866ed15cab1290cbce9b19d944
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2024-12-01 03:10:38 +0700

    Update miniBank.py

--- miniBank.py
+++ miniBank.py
@@ -2,5 +2,8 @@ class RekeningBank:
     def __init__(self, nama, saldo):
         self.nama = nama
         self.saldo = saldo
+    def simpan_uang(self, jumlah):
+        self.saldo += jumlah
+        print(f"Saldo {self.nama} bertambah {jumlah}. Saldo saat ini: {self.saldo}")
 
-
+    
