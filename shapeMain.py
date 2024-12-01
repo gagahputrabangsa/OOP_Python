b2b049a54f256f0dba6f73da227fe658572501e4
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2024-12-02 04:21:17 +0700

    using triangle

--- shapeMain.py
+++ shapeMain.py
@@ -1,3 +1,6 @@
 from shapes import Triangle, Square, Rectangle, Circle, Parallelogram
 # Usage
 print("==========================\nNama: Urname\nNim: 064xxx\n==========================\nPraktikum 10\n==========================")
+triangle = Triangle(5, 8)
+print("Luas Segitiga:", triangle.calculate_area())
+
