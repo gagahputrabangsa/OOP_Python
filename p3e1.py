5bcdccb88ae2e14989413d77e9b5328df27f777e
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2024-12-03 12:26:05 +0700

    create func init

--- p3e1.py
+++ p3e1.py
@@ -1,2 +1,10 @@
 class Mahasiswa:
-    
+    def __init__(self, nama, nim, fakultas, hobi):
+        self.nama = nama
+        self.nim = nim
+        self.fakultas = fakultas
+        self.hobi = hobi
+        
+
+
+   
