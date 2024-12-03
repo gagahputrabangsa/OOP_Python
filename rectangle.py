4b2c05cb560f400c5c056c8bd64a48799122cfbd
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2024-12-04 01:54:20 +0700

    Update rectangle.py

--- rectangle.py
+++ rectangle.py
@@ -3,7 +3,7 @@ class Mahasiswa:
     def __init__(self):
         self.panjang = 0
         self.lebar = 0
-        self.nama = "Gagah Putra Bangsa"
-        self.nim = "064002100036"
-        self.fakultas = "Teknik Industri"
-        self.hobi = "Membaca/Belajar"
+        self.nama = "Urname"
+        self.nim = "064xxxxxx"
+        self.fakultas = "Industrial Faculty"
+        self.hobi = "Reading"
