5f937952cd8d23ca41cf42cc4ec473587471d3e1
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2024-12-05 02:03:20 +0700

    constructor

--- friends_data.py
+++ friends_data.py
@@ -3,3 +3,9 @@ class Student:
     # Deklarasi variabel (instance & class)
     data_pribadi = "--- Data Pribadi ---"  # variabel class
 
+    # Constructor untuk inisialisasi objek Student
+    def __init__(self, nama):
+        self.nama = nama
+        self.nim = None
+        self.nilai = None
+
