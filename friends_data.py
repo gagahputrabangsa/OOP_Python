fc36bc35aca738fb74777a8b566d771f6e34d86a
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2024-12-05 02:03:59 +0700

    Update friends_data.py

--- friends_data.py
+++ friends_data.py
@@ -3,9 +3,24 @@ class Student:
     # Deklarasi variabel (instance & class)
     data_pribadi = "--- Data Pribadi ---"  # variabel class
 
-    # Constructor untuk inisialisasi objek Student
+    # Constructor
     def __init__(self, nama):
         self.nama = nama
         self.nim = None
         self.nilai = None
 
+    # Method 
+    def set_nim(self, nim):
+        self.nim = nim
+
+    # Method 
+    def set_nilai(self, nilai):
+        self.nilai = nilai
+
+    # Method 
+    def print_data(self):
+        print("Nama:", self.nama)
+        print("NIM:", self.nim)
+        print("Nilai:", self.nilai)
+
+    
