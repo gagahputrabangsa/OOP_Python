51e497dda2cd3e63f35b4a2598c1ce3c2c962c04
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2024-12-06 00:29:54 +0700

    main func

--- list_film.py
+++ list_film.py
@@ -1,2 +1,9 @@
 class Prak5e1:
     
+def main():
+        films = []
+        for i in range(5):
+            film = input("Film favorit KE-{}: ".format(i+1))
+            films.append(film)
+        
+        
