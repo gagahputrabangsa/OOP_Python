56b7a492ca9e5e76fdbb8fe39aa9aac5463629cf
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2024-12-07 03:09:11 +0700

    Update film2.py init , person

--- film2.py
+++ film2.py
@@ -1,2 +1,11 @@
 class Film:
     
+def __init__(self, film, year, director):
+        self.film = film
+        self.year = year
+        self.director = director
+
+    def person(self, name, id):
+        return f'Prak 5 ({name} - {id})\n-------------------ELKOM 2-------------------'
+
+
