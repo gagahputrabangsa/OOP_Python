e5c90e6e59fa37a1434dfecbf9d0e9f4dd90d3ee
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2024-12-10 06:12:44 +0700

    init func

--- moreDog.py
+++ moreDog.py
@@ -1,2 +1,8 @@
 class Dog:
 
+    def __init__(self, nama, jenis=None, umur=None, warna=None):
+        self.nama = nama
+        self.jenis = jenis
+        self.umur = umur
+        self.warna = warna
+
