0433603a1071423d2523ce560b381b805165e477
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2024-12-11 13:06:15 +0700

    other classes

--- p8e1.py
+++ p8e1.py
@@ -14,3 +14,21 @@ class Dosen(Awal):
         print("Golongan", self.golongan, "Mendapatkan Gaji:", self.gaji + 300000)
 
 
+class Staff(Awal):
+    def __init__(self, gaji, golongan):
+        super().__init__(gaji, golongan)
+
+    def print_gaji(self):
+        print("Golongan", self.golongan, "Mendapatkan Gaji:", self.gaji * 2)
+
+
+class Lainnya(Awal):
+    def __init__(self, gaji, golongan):
+        super().__init__(gaji, golongan)
+
+    def print_gaji(self):
+        print("Golongan", self.golongan, "Mendapatkan Gaji:", self.gaji)
+
+
+
+
