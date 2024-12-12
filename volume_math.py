a9abfcecca3589636fd81a42ede0c2508e70742b
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2024-12-12 21:34:36 +0700

    create volume class

--- volume_math.py
+++ volume_math.py
@@ -1,1 +1,13 @@
 import math
+
+class Volume:
+    def hitung_volume(self, sisi=None, panjang=None, lebar=None, tinggi=None, jari_jari=None):
+        if sisi is not None:
+            return sisi ** 3  # Volume kubus
+        elif panjang is not None and lebar is not None and tinggi is not None:
+            return panjang * lebar * tinggi  # Volume balok
+        elif jari_jari is not None and tinggi is not None:
+            return math.pi * jari_jari ** 2 * tinggi  # Volume tabung
+        else:
+            return "Parameter tidak valid"
+
