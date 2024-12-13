908fa93735f6ea689278c0c7ac25629911db3f54
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2024-12-13 20:50:04 +0700

    Update grade.py grade logic

--- part1/grade.py
+++ part1/grade.py
@@ -6,3 +6,24 @@ class gradeval:
     return name,id
 
   
+def get_grade(self):
+    if self.value >= 80:
+      return "A"
+    elif self.value >= 77:
+      return "A-"
+    elif self.value >= 74:
+      return "B+"
+    elif self.value >= 68:
+      return "B"
+    elif self.value >= 65:
+      return "B-"
+    elif self.value >= 62:
+      return "C+"
+    elif self.value >= 56:
+      return "C"
+    elif self.value >= 45:
+      return "D"
+    else:
+      return "E"
+
+
