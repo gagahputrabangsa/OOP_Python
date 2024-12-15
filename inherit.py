e8ce275f9ca31fcf3edcaab213f1fca38155dcf3
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2024-12-15 18:12:03 +0700

    Update inherit.py

--- inherit.py
+++ inherit.py
@@ -39,3 +39,28 @@ class Bola(BangunRuang):
     def volume(self):
         return (4/3) * math.pi * self.jari_jari ** 3
 
+class Silinder(BangunRuang):
+    def __init__(self, jari_jari, tinggi):
+        self.jari_jari = jari_jari
+        self.tinggi = tinggi
+
+    def luas(self):
+        return 2 * math.pi * self.jari_jari * (self.jari_jari + self.tinggi)
+
+    def volume(self):
+        return math.pi * self.jari_jari ** 2 * self.tinggi
+
+class PrismaSegitiga(BangunRuang):
+    def __init__(self, alas, tinggi, tinggi_prisma):
+        self.alas = alas
+        self.tinggi = tinggi
+        self.tinggi_prisma = tinggi_prisma
+
+    def luas(self):
+        return (self.alas * self.tinggi) + (3 * self.alas * self.tinggi_prisma)
+
+    def volume(self):
+        return (1/2) * self.alas * self.tinggi * self.tinggi_prisma
+
+
+
