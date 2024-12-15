7f8ca29ed8f2548b87af907d1759941a217f349c
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2024-12-15 18:11:51 +0700

    Update inherit.py

--- inherit.py
+++ inherit.py
@@ -1,2 +1,41 @@
 import math
 
+class BangunRuang:
+    def luas(self):
+        pass
+
+    def volume(self):
+        pass
+
+class Kubus(BangunRuang):
+    def __init__(self, sisi):
+        self.sisi = sisi
+
+    def luas(self):
+        return 6 * self.sisi ** 2
+
+    def volume(self):
+        return self.sisi ** 3
+
+class Balok(BangunRuang):
+    def __init__(self, panjang, lebar, tinggi):
+        self.panjang = panjang
+        self.lebar = lebar
+        self.tinggi = tinggi
+
+    def luas(self):
+        return 2 * (self.panjang * self.lebar + self.panjang * self.tinggi + self.lebar * self.tinggi)
+
+    def volume(self):
+        return self.panjang * self.lebar * self.tinggi
+
+class Bola(BangunRuang):
+    def __init__(self, jari_jari):
+        self.jari_jari = jari_jari
+
+    def luas(self):
+        return 4 * math.pi * self.jari_jari ** 2
+
+    def volume(self):
+        return (4/3) * math.pi * self.jari_jari ** 3
+
