969dbcf2bdaf7097d3020efbf9a22df2b5fcf554
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2024-12-16 18:58:29 +0700

    Update and rename pyside_student.py to studentInfo.py

---
+++ pyside/studentInfo.py
@@ -0,0 +1,4 @@
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QVBoxLayout, QWidget, QLineEdit, QMessageBox
+from PySide6.QtGui import QFont
+from PySide6.QtCore import QSize
+
