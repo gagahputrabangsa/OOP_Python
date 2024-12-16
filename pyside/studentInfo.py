5633520aabde751248378111e8d31543ed657aa5
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2024-12-16 18:58:50 +0700

    Update studentInfo.py class

--- pyside/studentInfo.py
+++ pyside/studentInfo.py
@@ -2,3 +2,42 @@ from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QV
 from PySide6.QtGui import QFont
 from PySide6.QtCore import QSize
 
+class CustomWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.layout = QVBoxLayout(self)
+
+        self.label = QLabel("Masukkan detail Anda:")
+        self.label.setFont(QFont('Arial'))
+
+        self.name_textbox = QLineEdit()
+        self.name_textbox.setFont(QFont('Arial'))
+        self.name_textbox.setPlaceholderText("Nama")
+
+        self.nim_textbox = QLineEdit()
+        self.nim_textbox.setFont(QFont('Arial'))
+        self.nim_textbox.setPlaceholderText("NIM")
+
+        self.hobby_textbox = QLineEdit()
+        self.hobby_textbox.setFont(QFont('Arial'))
+        self.hobby_textbox.setPlaceholderText("Hobi")
+
+        self.button = QPushButton("Kirim")
+        self.button.setFont(QFont('Arial'))
+
+        self.reset_button = QPushButton("Reset")
+        self.reset_button.setFont(QFont('Arial'))
+
+        self.layout.addWidget(self.label)
+        self.layout.addWidget(self.name_textbox)
+        self.layout.addWidget(self.nim_textbox)
+        self.layout.addWidget(self.hobby_textbox)
+        self.layout.addWidget(self.button)
+        self.layout.addWidget(self.reset_button)
+        # self.setStyleSheet("background-color: #F5B7AA;")
+        self.button.clicked.connect(self.greet)
+        self.reset_button.clicked.connect(self.reset)
+        self.button.setStyleSheet("background-color: #82E0AA ;")
+        self.reset_button.setStyleSheet("background-color: #E9F5AA;")
+
