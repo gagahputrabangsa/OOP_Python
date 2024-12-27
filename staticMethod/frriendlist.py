529014dda0b5a6d5688d47cbe0f40a7548a79448
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2024-12-28 00:07:32 +0700

    Update frriendlist.py

--- staticMethod/frriendlist.py
+++ staticMethod/frriendlist.py
@@ -1,2 +1,31 @@
 class Student:
-   
+    # (instance & class)
+    data_pribadi = "--- Private Data ---"  # variabel class
+
+    # Constructor 
+    def __init__(self, nama):
+        self.nama = nama
+        self.nim = None
+        self.nilai = None
+
+    # Method 
+    def set_nim(self, nim):
+        self.nim = nim
+
+    # Method 
+    def set_nilai(self, nilai):
+        self.nilai = nilai
+
+    # Method 
+    def print_data(self):
+        print("Nama:", self.nama)
+        print("NIM:", self.nim)
+        print("Nilai:", self.nilai)
+
+    
+    @staticmethod
+    def print_data_teman(nomor_teman, teman):
+        print("--- Data Teman", nomor_teman, "---")
+        teman.print_data()
+
+
