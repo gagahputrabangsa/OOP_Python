4d8ee1f92dcbe56b60d7b432113d6f183ea50b24
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2025-01-26 20:02:05 +0700

    Update restaurant.py

--- restaurant.py
+++ restaurant.py
@@ -1,1 +1,12 @@
+class MenuItem:
+    def __init__(self, name, price, description):
+        self.name = name
+        self.price = price
+        self.description = description
+
+    def __str__(self):
+        return f"{self.name} - ${self.price:.2f} ({self.description})"
+
+
+
 
